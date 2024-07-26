e3c132f5e4e04a5dcee122faf899781582debb20
doshant <njrdoshantp@gmail.com>
2024-07-26 12:07:21 +0530

    day 1 of js  chai code

---
+++ day1/solution.js
@@ -0,0 +1,76 @@
+//Activity 1
+
+//Task 1
+var first = 99;
+console.log(first);
+
+//Task 2 
+let second = "I am the string";
+console.log(second);
+
+//Activity 2 
+
+//Task 3
+const third = true;
+console.log(third);
+
+//Activity 3
+
+//Task 4 
+let four = 44;
+let five = "i am the string";
+let six = false;
+let seven = {
+    name:"Dhone",
+    number:7,
+    nick:"thala"
+};
+let eight = [33,77,4,90];
+console.log(typeof(four));
+console.log(typeof(five));
+console.log(typeof(six));
+console.log(typeof(seven));
+console.log(typeof(eight));
+
+//Activity 4 
+
+//Task 5
+let nine = 55;
+console.log(nine);
+ nine = 77;
+console.log(nine);
+
+//Activity 5 
+
+//Task 6 
+const ten = 43;
+console.log(ten);
+//ten = 66 
+//we cant reassign a value to the variable which is declared with const 
+
+//variable types console log 
+let eleven = 33;
+let twelve = "i am the string";
+let thirten = false;
+let fourten = {
+      name: "messi",
+      age:33,
+      foot:"left"
+}
+let fiften = [33,44,"pour",88,"rub"]
+console.log(`the value of the variable eleven is ${eleven} and the type of the eleven is ${typeof(eleven)}`);
+console.log(`the value of the variable twelve is ${twelve} and the type of the twelve is ${typeof(twelve)}`);
+console.log(`the value of the variable thirten is ${thirten} and the type of the thirten is ${typeof(thirten)}`);
+console.log(`the value of the variable fourten is ${JSON.stringify(fourten)} and the type of the fourten is ${typeof(fourten)}`);
+console.log(`the value of the variable fiften is ${fiften} and the type of the fiften is ${typeof(fiften)}`);
+
+//Reassignment Demo 
+
+let sixten = 17;
+sixten = 46;
+console.log(sixten);
+const seventen = 19;
+//seventen = 22; we cant reassign the value if its const 
+console.log(seventen);
+
+//Completed 
